25fc3371211ede2c42e58767c5f6201ef5e28403
DangKhoi1 <khoiplbl@gmail.com>
2025-02-12 23:10:20 +0700

    Cai dat cac handler và truy xuat co so du lieu

--- app/config/index.js
+++ app/config/index.js
@@ -3,11 +3,12 @@ const config = {
     port: process.env.PORT || 3000,
   },
   db: {
-    uri: process.env.MONGODB.URI || "mongodb://127.0.0.1:27017/contactbook"
+    uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactbook"
   }
 };
 
-
 module.exports = config;
+
+
 
 
--- server.js
+++ server.js
@@ -25,4 +25,4 @@ async function startServer() {
     }
 }
 
-startServer()
+startServer();
